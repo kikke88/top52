d81aa2fc39037a7ff3804414afcbffada60627f0
kikke88 <kikke88@yandex.ru>
2021-02-09 18:51:09 +0300

    finally it works like on previous vm

--- config/puma.rb
+++ config/puma.rb
@@ -5,8 +5,8 @@ environment ENV['RACK_ENV'] || 'production'
 bind "unix:///var/www/top52/socket"
 directory "/var/www/top52"
 
-threads 4, 16
-#workers 1
+threads 1, 8
+workers 2
 
 stdout_redirect "/var/www/top52/shared/log/web_stdout.log", "/var/www/top52/shared/log/web_stderr.log", true
 pidfile "/var/www/top52/shared/puma.pid"
